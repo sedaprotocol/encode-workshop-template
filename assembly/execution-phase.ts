485e4f573784daaf03ffbeb013d8b6a124d74676
Thomas van Dam <thomas@seda.xyz>
2024-07-24 16:38:40 +0200

    chore(dr-wasm): replace console with Console import from as-sdk

--- assembly/execution-phase.ts
+++ assembly/execution-phase.ts
@@ -1,13 +1,13 @@
-import { Process } from "@seda-protocol/as-sdk/assembly";
+import { Process, Console } from "@seda-protocol/as-sdk/assembly";
 import { fetchBinance } from "./services/binance-service";
 import { fetchKucoin } from "./services/kucoin-service";
 import { fetchOkx } from "./services/okx-service";
-import { median } from './services/median';
+import { median } from "./services/median";
 
 export function executionPhase(): void {
   const drInputsRaw = String.UTF8.decode(Process.getInputs().buffer);
-  console.log('Fetching price for pair: ' + drInputsRaw);
-  const drInputs = drInputsRaw.split('-');
+  Console.log("Fetching price for pair: " + drInputsRaw);
+  const drInputs = drInputsRaw.split("-");
   const symbolA = drInputs[0];
   const symbolB = drInputs[1];
   const prices: f64[] = [];
@@ -15,30 +15,30 @@ export function executionPhase(): void {
   const binance = fetchBinance(symbolA, symbolB);
 
   if (!binance.success) {
-    console.error(binance.value);
+    Console.error(binance.value);
   } else {
     const price = f64.parse(binance.value);
-    console.log('Binance price: ' + price.toString());
+    Console.log("Binance price: " + price.toString());
     prices.push(price);
   }
 
   const kucoin = fetchKucoin(symbolA, symbolB);
 
   if (!kucoin.success) {
-    console.error(kucoin.value);
+    Console.error(kucoin.value);
   } else {
     const price = f64.parse(kucoin.value);
-    console.log('Kucoin price: ' + price.toString());
+    Console.log("Kucoin price: " + price.toString());
     prices.push(price);
   }
 
   const okx = fetchOkx(symbolA, symbolB);
 
   if (!okx.success) {
-    console.error(okx.value);
+    Console.error(okx.value);
   } else {
     const price = f64.parse(okx.value);
-    console.log('Okx price: ' + price.toString());
+    Console.log("Okx price: " + price.toString());
     prices.push(price);
   }
 
