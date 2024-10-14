45c7b61cc818dd18ab33edf0a0adf0f6cd5175ef
jasperdg <jasperdegooijer@gmail.com>
2024-10-14 21:14:36 +0100

    updated dependencies and added passing testcases

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,6 +1,7 @@
 import { afterEach, describe, it, expect, mock } from "bun:test";
 import { file } from "bun";
-import { executeDrWasm, executeTallyWasm } from "@seda-protocol/dev-tools"
+import { testOracleProgramExecution, testOracleProgramTally } from "@seda-protocol/dev-tools"
+import { BigNumber } from 'bignumber.js'
 
 const WASM_PATH = "build/debug.wasm";
 
@@ -11,27 +12,36 @@ afterEach(() => {
 });
 
 describe("data request execution", () => {
-  it("Should return hello, world", async () => {
-    const wasmBinary = await file(WASM_PATH).arrayBuffer();
+  it("should aggregate the results from the different APIs", async () => {
+    fetchMock.mockImplementation((url) => {
+      if (url.host === "api.binance.com") {
+        return new Response(JSON.stringify({ price: "2452.30000" }));
+      }
 
-    const vmResult = await executeDrWasm(
-      Buffer.from(wasmBinary),
-      Buffer.from(""),
+      return new Response('Unknown request');
+    });
+
+    const oracleProgram = await file(WASM_PATH).arrayBuffer();
+
+    const vmResult = await testOracleProgramExecution(
+      Buffer.from(oracleProgram),
+      Buffer.from("eth-usdc"),
       fetchMock
     );
 
     expect(vmResult.exitCode).toBe(0);
-
-    const result = vmResult.resultAsString;
-    expect(result).toEqual("Hello, world");
+    // BigNumber.js is big endian
+    const hex = Buffer.from(vmResult.result.toReversed()).toString('hex');
+    const result = BigNumber(`0x${hex}`);
+    expect(result).toEqual(BigNumber('2452300032'));
   });
 
-  it('should also return hello, world from tally', async () => {
-    const wasmBinary = await file(WASM_PATH).arrayBuffer();
+  it('should tally all results in a single data point', async () => {
+    const oracleProgram = await file(WASM_PATH).arrayBuffer();
 
     // Result from the execution test
-    let buffer = Buffer.from([72, 101, 108, 108, 111, 44, 32, 119, 111, 114, 108, 100]);
-    const vmResult = await executeTallyWasm(Buffer.from(wasmBinary), Buffer.from('tally-inputs'), [{
+    let buffer = Buffer.from([0, 33, 43, 146, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+    const vmResult = await testOracleProgramTally(Buffer.from(oracleProgram), Buffer.from('tally-inputs'), [{
       exitCode: 0,
       gasUsed: 0,
       inConsensus: true,
@@ -39,7 +49,6 @@ describe("data request execution", () => {
     }]);
 
     expect(vmResult.exitCode).toBe(0);
-    const result = vmResult.resultAsString;
-    expect(result).toEqual("Hello, world");
+    expect(vmResult.resultAsString).toEqual('2452300032');
   });
 });
